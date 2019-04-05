e496896d01803a22bb81e400733471c6659bf2a9
Deepali2 <hdgarg@gmail.com>
2019-04-04 17:21:40 -0700

    starts the algorithm

--- tree questions/createLevelLinkedList.js
+++ tree questions/createLevelLinkedList.js
@@ -5,29 +5,70 @@
 //So we need to convert the given binary tree to a linked list of all the nodes at each deapth. 
 //So if we have a binary tree with depth d we will have d linked lists.
 
-//constructor to create a binary tree
+//modify the pre-order traversal algorithm by passing an extra argument of the current height(level)ß + 1
 
-class BinaryTree {
+//create a binary tree
+function BinaryTree () {
+  this.root = null;
+}
+function Node(val) {
+  this.data = val;
+  this.children = [];
+}
+node7 = new Node(7);
+node1 = new Node(1);
+node2 = new Node(2);
+node1another = new Node(1);
+node2another = new Node(2);
+node5 = new Node(5);
+node8 = new Node(8);
+node9 = new Node(9);
+node11 = new Node(11);
+
+node7.children.push(node1);
+node1.children.push(node2, node1another);
+node1another.children.push(node2another);
+node5.children.push(node8, node9);
+node9.children.push(node11);
+
+//create a binary Search Tree
+class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  //insert a Node 
-  insertNode(value) {
+
+  //insert a node
+  insertANode(val) {
     let node = {
-      data: value,
-      children: []
-    };
+      data: val,
+      left: null, 
+      right: null
+    }
     if (!this.root) {
       this.root = node;
-      return (`node added as the root of the empty tree`);
+      return (`The value has been added as the root of the binary search tree`);
     }
-    
+    let currentNode = this.root;
+    while(currentNode) {
+      if (val < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = node;
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else if (val > currentNode.data) {
+        if (!currentNode.right) {
+          currentNode.right = node;
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      } else {
+
+      }
+    }
+     
   }
-
-
-
 }
 
-//create an instance of the binary tree
-const BT1 = new BinaryTree();
-console.log(BT1.insertNode(3));
