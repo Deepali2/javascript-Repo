c80d6b235b9c8b322349ab0092756e7481e01eec
Deepali2 <hdgarg@gmail.com>
2019-04-04 17:56:03 -0700

    finds correcttly the number of moments for which all bulbs that have been switched on will light up

--- tree questions/bulbsShine.js
+++ tree questions/bulbsShine.js
@@ -1,45 +1,18 @@
-//helper function to make an object for each bulb
-function bulbObj(val) {
-  let bulb = {
-      moment: val,
-      on: 0,
-      light: 0
-  }
-  return bulb;
-}
+//Bulbs are connected in series with the first bulb connected to power outlet. Think of an array as representing the moments in time when a bulb is switched on.
+//bulb will light up only if all the previous bulbs are also on. Determine the moments in time when all bulbs that have been switched on will light up together.
+//Example: [2, 1, 3, 5, 4] should return 3 corresponding to moments 2, 3, 5
 
 function solution(A) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  let bulbs = [];
-
-  //loop over the input array, create an object for each bulb, push it into bulbs
-  for (let ele of A) {
-    let obj = bulbObj(ele);
-      bulbs.push(obj);
-  }
-
-  //loop to see which bulbs will turn on.
-  //keep track of moments when all bulbs that are on are lighted.
-  //return the moments
   let momentsAllOn = 0;
-  for (let i = 0; i < bulbs.length; i++) {
-    let lightFactor = 1;
-    for (let j = 0; j <= i; j++) {
-      if (bulbs[j].light === 0) lightFactor = 0;
-      if (bulbs[j].moment === i + 1) {
-        bulbs[j].on = 1;
-        if (lightFactor === 1) {
-          bulbs[j].light = 1;
-          momentsAllOn++;
-          for (k = j; k < bulbs.length; k++) {
-            if (bulbs[k].on === 1) bulbs[k].light = 1;
-          }
-        }
-      }
+  let maxSoFar = A[0];
+  for (let i = 0; i < A.length; i++) {
+    console.log(maxSoFar)
+    if (A[i] >= maxSoFar) {
+      momentsAllOn++;
+      maxSoFar = A[i];
     }
   }
   return momentsAllOn;
-  
 }
 
 console.log(solution([2, 1, 3, 5, 4]));
