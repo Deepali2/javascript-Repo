b0426b66841daa8f1041156853c2218c659398ba
Deepali2 <hdgarg@gmail.com>
2019-04-10 12:44:52 -0700

    adds the binary tree and the binary search tree along with the traversal algorithms

--- tree questions/createLevelLinkedList.js
+++ tree questions/createLevelLinkedList.js
@@ -8,30 +8,80 @@
 //modify the pre-order traversal algorithm by passing an extra argument of the current height(level)ß + 1
 
 //create a binary tree
-function BinaryTree () {
+//A binary tree will have a maximum of two children per node but the values need not be in sorted order
+function Tree() {
   this.root = null;
 }
+
 function Node(val) {
   this.data = val;
-  this.children = [];
+  this.left = null;
+  this.right = null;
 }
-node7 = new Node(7);
-node1 = new Node(1);
-node2 = new Node(2);
-node1another = new Node(1);
-node2another = new Node(2);
-node5 = new Node(5);
-node8 = new Node(8);
-node9 = new Node(9);
-node11 = new Node(11);
 
-node7.children.push(node1);
-node1.children.push(node2, node1another);
-node1another.children.push(node2another);
-node5.children.push(node8, node9);
-node9.children.push(node11);
+let tree1 = new Tree();
+let node7 = new Node(7);
+tree1.root = node7;
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node1another = new Node(1);
+let node2another = new Node(2);
+let node5 = new Node(5);
+let node8 = new Node(8);
+let node9 = new Node(9);
+let node11 = new Node(11);
+
+node7.left = node1;
+node7.right = node5;
+node1.left = node2;
+node1.right = node1another;
+node1another.left = node2another;
+node5.left = node8;
+node5.right = node9;
+node9.left = node11;
+
+function preorderTree(root) {
+  //root left right
+  console.log(root.data);
+  if (root.left) preorderTree(root.left);
+  if(root.right) preorderTree(root.right);
+
+}
+
+function inorderTree(root) {
+  //left root right
+  if (root.left) inorderTree(root.left);
+  console.log(root.data);
+  if(root.right)inorderTree(root.right);
+}
+
+function postorderTree(root) {
+  //left right root
+  if (root.left) postorderTree(root.left);
+  if (root.right) postorderTree(root.right);
+  console.log(root.data);
+}
+
+function breadthTraverseTree(tree) {
+  let queue = [];
+  let temp;
+  if(!tree.root) return ('The tree is empty');
+  queue.push(tree.root);
+  while(queue.length) {
+    temp = queue.splice(0, 1)[0];
+    console.log(temp.data);
+    if(temp.left) queue.push(temp.left);
+    if(temp.right) queue.push(temp.right);
+  }
+}
+
+preorderTree(tree1.root);
+inorderTree(tree1.root);
+postorderTree(tree1.root);
+breadthTraverseTree(tree1);
 
 //create a binary Search Tree
+//A binary search tree is a special type of a binary tree in which the values are in order
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -46,7 +96,7 @@ class BinarySearchTree {
     }
     if (!this.root) {
       this.root = node;
-      return (`The value has been added as the root of the binary search tree`);
+      return(`The value ${val} has been added as the root`)
     }
     let currentNode = this.root;
     while(currentNode) {
@@ -65,10 +115,60 @@ class BinarySearchTree {
           currentNode = currentNode.right;
         }
       } else {
-
+       return (`This value already exists in the binary search tree`);
       }
+    }  
+    return (`The value ${val} has been added`);   
+  }
+
+  //Depth first traversal:
+  //a) inorder: left root right
+  //b) preorder: root left right 
+  //c) postorder: left right root
+
+  inorder(root) {
+    if(root.left) this.inorder(root.left);
+    console.log(root.data);
+    if(root.right) this.inorder(root.right);
+  }
+
+  preorder(root) {
+    console.log(root.data);
+    if(root.left) this.preorder(root.left);
+    if(root.right) this.preorder(root.right);
+  }
+
+  postorder(root) {
+    if (root.left) this.postorder(root.left);
+    if(root.right) this.postorder(root.right);
+    console.log(root.data);
+  }
+
+  breadthTraverse(tree) {
+    let queue = [];
+    let temp;
+    queue.push(tree.root);
+    while(queue.length) {
+      //dequeue the queue
+      temp = queue.splice(0, 1)[0];
+      console.log(temp.data);
+      if(temp.left) queue.push(temp.left);
+      if(temp.right) queue.push(temp.right);
     }
-     
   }
 }
+
+const BST1 = new BinarySearchTree();
+console.log(BST1.insertANode(3));
+console.log(BST1.insertANode(1));
+console.log(BST1.insertANode(5));
+console.log(BST1.insertANode(4));
+console.log(BST1.insertANode(0));
+console.log(BST1.insertANode(8));
+console.log(BST1.insertANode(2));
+console.log(BST1.insertANode(10));
+console.log(BST1.inorder(BST1.root));
+console.log(BST1.preorder(BST1.root));
+console.log(BST1.postorder(BST1.root));
+console.log(BST1.breadthTraverse(BST1));
 
